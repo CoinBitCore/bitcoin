1a5414e60849b224d279cdb8cfa70fa3ebe2984a
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2018-03-24 21:22:37 +0200

    fix gitian

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -233,7 +233,7 @@ if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
     git clone https://github.com/bitcoin-core/gitian.sigs.git
-    git clone https://github.com/bitcoin-core/coinbit-detached-sigs.git
+    git clone https://github.com/bitcoin-core/bitcoin-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
