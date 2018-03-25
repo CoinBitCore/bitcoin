6a1a68aca2d437e7a5d579ec8a2223bcb12b0638
Maksym Shekhovtsev <maksym.shekhovtsev@gmail.com>
2018-03-25 05:17:54 +0300

    gitian

--- contrib/gitian-build.sh
+++ contrib/gitian-build.sh
@@ -232,8 +232,8 @@ echo ${COMMIT}
 if [[ $setup = true ]]
 then
     sudo apt-get install ruby apache2 git apt-cacher-ng python-vm-builder qemu-kvm qemu-utils
-    git clone https://github.com/coinbit-core/gitian.sigs.git
-    git clone https://github.com/coinbit-core/coinbit-detached-sigs.git
+    git clone https://github.com/bitcoin-core/gitian.sigs.git
+    git clone https://github.com/bitcoin-core/bitcoin-detached-sigs.git
     git clone https://github.com/devrandom/gitian-builder.git
     pushd ./gitian-builder
     if [[ -n "$USE_LXC" ]]
